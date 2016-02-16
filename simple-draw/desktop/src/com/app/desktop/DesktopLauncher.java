71bb7842154f257e24a6c8ea539fc7edfee46594
ferrolho <henriqueferrolho@gmail.com>
2016-02-16 11:56:41 +0000

    Starting SimpleDraw

--- simple-draw/desktop/src/com/app/desktop/DesktopLauncher.java
+++ simple-draw/desktop/src/com/app/desktop/DesktopLauncher.java
@@ -1,13 +1,29 @@
 package com.app.desktop;
 
+import com.app.SimpleDraw;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.app.SimpleDraw;
 
 public class DesktopLauncher {
 
 	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "Simple Draw";
+
+		// Dimension screenDimension =
+		// Toolkit.getDefaultToolkit().getScreenSize();
+		// config.width = screenDimension.width;
+		// config.height = screenDimension.height;
+
+		// fullscreen
+		// config.fullscreen = true;
+
+		// vSync
+		config.vSyncEnabled = true;
+
+		// Multisample anti-aliasing
+		config.samples = 4;
 
 		new LwjglApplication(new SimpleDraw(), config);
 	}
