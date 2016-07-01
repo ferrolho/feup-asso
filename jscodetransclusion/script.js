38a5328276fb6ba77fab02ef993eabf94dd89211
Joao Norim <norim_13@hotmail.com>
2016-07-02 00:03:42 +0100

    Method parsing minding comments (single line and multi line).

--- jscodetransclusion/script.js
+++ jscodetransclusion/script.js
@@ -2,29 +2,29 @@
 
 jQuery(document).ready(function() {
 
-	jQuery(".js-code").each(function() {
-		var thisElement = this;
+    jQuery(".js-code").each(function() {
+        var thisElement = this;
 
-		var rawLink = jQuery(thisElement).attr('data-url');
+        var rawLink = jQuery(thisElement).attr('data-url');
         var error = false;
 
-		var linkTokens = rawLink.split('@');
+        var linkTokens = rawLink.split('@');
 
-		var link = linkTokens[0];
-		var methodName = linkTokens[1];
+        var link = linkTokens[0];
+        var methodName = linkTokens[1];
 
-		var link_split = link.split('github.com');
-		if(link_split.length > 1){
-			link = 'https://raw.githubusercontent.com'+link_split[1];
-			link_split = link.split('blob/');
-			if (link_split.length < 2) 
-				error = true;
-			else{
-				link = link_split[0] + link_split[1];
-				
-			}
-			
-		}
+        var link_split = link.split('github.com');
+        if(link_split.length > 1){
+            link = 'https://raw.githubusercontent.com'+link_split[1];
+            link_split = link.split('blob/');
+            if (link_split.length < 2) 
+                error = true;
+            else{
+                link = link_split[0] + link_split[1];
+                
+            }
+            
+        }
         else{
             link_split = link.split('raw.githubusercontent.com');
             if (link_split.length < 2)
@@ -34,12 +34,15 @@ jQuery(document).ready(function() {
 
         if(error)
             badGitHubLink(thisElement);
-		else{
+        else{
             jQuery.ajax({
                 url: link,
                 success: function(data) {
                     if (methodName) {
-                        jQuery(thisElement).text(getMethodFromFileString(data, methodName));
+                        var method_string = getMethodFromFileString(data, methodName);
+                        if(method_string == null)
+                            method_string = 'Method not found...';
+                        jQuery(thisElement).text(method_string);
                     }
                     else{
                         jQuery(thisElement).text(data);
@@ -47,14 +50,14 @@ jQuery(document).ready(function() {
                     hljs.highlightBlock(thisElement);
                 }
             });
-		}	
+        }   
 
-	});
+    });
 
 });
 
 function badGitHubLink(elem){
-	jQuery(elem).text('Bad GitHub link...');
+    jQuery(elem).text('Bad GitHub link...');
 }
 
 function getMethodFromFileString(fileString, methodName){
@@ -63,14 +66,18 @@ function getMethodFromFileString(fileString, methodName){
     var re = new RegExp(regex_string, "g");
 
     var method_start_index = fileString.search(re); 
+    if(method_start_index == -1)
+        return null;
+
     var method_body_start_index = fileString.indexOf('{', method_start_index);
 
-    var stack = [];
     var current_char;
     var i = method_body_start_index+1; //skipping the first char: '{'
     var open_curlies = 1; // number of 'openning' curly brackets not closed
     var single_quote_opened = false;
     var double_quote_opened = false;
+    var single_line_comment_opened = false;
+    var multi_line_comment_opened = false;
 
     for( ; i < fileString.length; i++){
         if(open_curlies == 0)
@@ -78,27 +85,55 @@ function getMethodFromFileString(fileString, methodName){
         current_char = fileString.charAt(i);
         switch(current_char){
             case '\'' : 
-                if(double_quote_opened) break; // if double quote is opened, this char (') is part of the string
+                if(double_quote_opened || single_line_comment_opened || multi_line_comment_opened) 
+                    break; // if double quote is opened, this char (') is part of the string
                 if(single_quote_opened && fileString.charAt(i-1) != '\\') 
                     single_quote_opened = false;
                 else single_quote_opened = true;
                 break;
 
             case '"' : 
-                if(single_quote_opened) break; // if single quote is opened, this char (") is part of the string
+                if(single_quote_opened || single_line_comment_opened || multi_line_comment_opened) 
+                    break; // if single quote is opened, this char (") is part of the string
                 if(double_quote_opened && fileString.charAt(i-1) != '\\') 
                     double_quote_opened = false;
                 else double_quote_opened = true;
                 break;
 
             case '{' : 
-                if(single_quote_opened || double_quote_opened) break; //if inside a string, ignore curly brackets
+                if(single_quote_opened || double_quote_opened || single_line_comment_opened || multi_line_comment_opened) 
+                    break; //if inside a string, ignore curly brackets
                 open_curlies++;
                 break;
 
             case '}' : 
-                if(single_quote_opened || double_quote_opened) break; //if inside a string, ignore curly brackets
+                if(single_quote_opened || double_quote_opened || single_line_comment_opened || multi_line_comment_opened) 
+                    break; //if inside a string, ignore curly brackets
                 open_curlies--;
+                break;
+
+            case '/' :
+                if(single_quote_opened || double_quote_opened)
+                    break; 
+                if(fileString.charAt(i-1) == '/'){
+                    single_line_comment_opened = true;
+                }
+                else if (fileString.charAt(i+1) == '*'){
+                    multi_line_comment_opened = true;
+                    i++;
+                }
+                break;
+
+            case '\n' :
+                if(single_line_comment_opened)
+                    single_line_comment_opened = false;
+                break;
+
+            case '*' : 
+                if(multi_line_comment_opened && fileString.charAt(i+1) == '/'){
+                    multi_line_comment_opened = false;
+                    i++;
+                }
                 break;
 
             default: break;
@@ -106,5 +141,4 @@ function getMethodFromFileString(fileString, methodName){
 
     };
     return fileString.substring(method_start_index, i);
-
 };
