73ed93f3fc9d0edac593823f91a2fec6d3cb3fd1
Joao Norim <norim_13@hotmail.com>
2016-07-01 22:49:24 +0100

    Method extraction from file done right (or at least, better).

--- jscodetransclusion/script.js
+++ jscodetransclusion/script.js
@@ -12,36 +12,71 @@ jQuery(document).ready(function() {
 		var link = linkTokens[0];
 		var methodName = linkTokens[1];
 
-		if (methodName) {
-			loadScript(link, function() {
-				jQuery(thisElement).text(window[methodName].toString());
-				hljs.highlightBlock(thisElement);
-			});
-		} else {
-			jQuery.ajax({
-				url: link,
-				success: function(data) {
-					jQuery(thisElement).text(data);
-					hljs.highlightBlock(thisElement);
+		jQuery.ajax({
+			url: link,
+			success: function(data) {
+				if (methodName) {
+					jQuery(thisElement).text(getMethodFromFileString(data, methodName));
 				}
-			});
-		}
+				else{
+					jQuery(thisElement).text(data);
+				}
+				hljs.highlightBlock(thisElement);
+			}
+		});
+
 	});
 
 });
 
-function loadScript(url, callback) {
-    // Adding the script tag to the head as suggested before
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url;
+function getMethodFromFileString(fileString, methodName){
 
-    // Then bind the event to the callback function.
-    // There are several events for cross browser compatibility.
-    script.onreadystatechange = callback;
-    script.onload = callback;
+    var regex_string = 'function(\\s)+'+methodName+'(\\s)*\\([A-Za-z\\s_0-9,]*\\)(\\s)*';
+    var re = new RegExp(regex_string, "g");
 
-    // Fire the loading
-    head.appendChild(script);
-}
+    var method_start_index = fileString.search(re); 
+    var method_body_start_index = fileString.indexOf('{', method_start_index);
+
+    var stack = [];
+    var current_char;
+    var i = method_body_start_index+1; //skipping the first char: '{'
+    var open_curlies = 1; // number of 'openning' curly brackets not closed
+    var single_quote_opened = false;
+    var double_quote_opened = false;
+
+    for( ; i < fileString.length; i++){
+        if(open_curlies == 0)
+            break;
+        current_char = fileString.charAt(i);
+        switch(current_char){
+            case '\'' : 
+                if(double_quote_opened) break; // if double quote is opened, this char (') is part of the string
+                if(single_quote_opened && fileString.charAt(i-1) != '\\') 
+                    single_quote_opened = false;
+                else single_quote_opened = true;
+                break;
+
+            case '"' : 
+                if(single_quote_opened) break; // if single quote is opened, this char (") is part of the string
+                if(double_quote_opened && fileString.charAt(i-1) != '\\') 
+                    double_quote_opened = false;
+                else double_quote_opened = true;
+                break;
+
+            case '{' : 
+                if(single_quote_opened || double_quote_opened) break; //if inside a string, ignore curly brackets
+                open_curlies++;
+                break;
+
+            case '}' : 
+                if(single_quote_opened || double_quote_opened) break; //if inside a string, ignore curly brackets
+                open_curlies--;
+                break;
+
+            default: break;
+        }
+
+    };
+    return fileString.substring(method_start_index, i);
+
+};
